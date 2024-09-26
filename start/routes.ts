d23412e50b29653e6a87dabf6c751d4af525e166
Leandro Barbosa <101986070+LeandroBarbosa753@users.noreply.github.com>
2024-09-26 12:59:07 +0000

    :lock: chore: Adicionando rotas e configurando middleware de autenticação

---
+++ start/routes.ts
@@ -0,0 +1,10 @@
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.post('/session', 'SessionsController.store')
+Route.delete('/session', 'SessionsController.destroy').middleware('auth')
+
+Route.resource('/user', 'UsersController').apiOnly()
+Route.group(() => {
+  Route.resource('/task', 'TasksController').apiOnly()
+}).middleware('auth')
+
